e761c7f0eff0ee625409f0983765286f682ae314
GreatJack <wyre@gmail.com>
2017-05-18 14:36:12 -0400

    Increase effeciency of the program by around 10x

--- src/com/wyre/Main.java
+++ src/com/wyre/Main.java
@@ -13,20 +13,35 @@ public class Main {
 
     public static void main(String[] args) {
         // loop through all latitudes
-        for (double lat = 40.00; lat < 41.00; lat = lat + 1) { // use a .1 stepping counter
+        for (double lat = -90.00; lat < 90.00; lat = lat + 1) { // use a .1 stepping counter
             //loop through all longitudes
-            for (double lon = -70.00; lon <= -69.90; lon = lon + .1) { //use a .1 stepping counter so that we dont miss any shuls
+            for (double lon = -180.00; lon <= 180.90; lon = lon + .1) { //use a .1 stepping counter so that we dont miss any shuls
                 System.out.println("Getting results from go daven for lat:" + lat + " Lon: " + lon);
                 try {
+                    //use my JDaven Api to get the minyanim and iterate through tehm
                     JDaven jd = new JDaven(lat, lon, 20);
                     if (jd.getMinyanim() != null) {
                         System.out.println("Number of results for these values is: " + jd.getMinyanim().length);
                         for (minyanim mi : jd.getMinyanim()) {
-                            System.out.println("Address of shul is: " + mi.getAddress() + mi.getCity() + mi.getState());
+                            System.out.println("Address of shul is: " + mi.getAddress() +" " + mi.getCity() +" "+ mi.getState());
                             MinyanEntry temp = new MinyanEntry();
                             temp.setName(mi.getName());
                             temp.setAddress(mi.getAddress() + " " + mi.getCity() + " " + mi.getState());
-                            //get the latitude and longitude from the neomatin api
+                            //check whether we already have this shul in the database and if we do then skip
+                            boolean contains = false;
+                            for (MinyanEntry entry : me) {
+                                //check if any entry is the same as temp
+                                if (entry.getAddress().equals(temp.getAddress())) {
+                                    contains = true;
+                                    break;
+                                }
+                            }
+
+                            if(contains==true){ //since the shul is allready on the list, skip to the next iteration of the for loop
+                                continue;
+                            }
+
+                            //get the latitude and longitude bounding box from the neomatin api
                             NeomatinGetter ng = new NeomatinGetter(temp.getAddress());
                             temp.setMinLon(ng.getMinLon());
                             temp.setMaxLon(ng.getMaxLon());
@@ -37,17 +52,9 @@ public class Main {
                                System.out.println("Some values are null, skipping this shul");
                                 continue;
                             }
-                            boolean contains = false;
-                            for (MinyanEntry entry : me) {
-                                //check if any entry is the same as temp
-                                if (entry.equals(temp)) {
-                                    contains = true;
-                                    break;
-                                }
-                            }
-                            if(contains==false){
-                                me.add(temp);
-                            }
+                            //add the minyan to the list
+                            System.out.println("Adding " + temp.getName() +"  to the list of shuls");
+                           me.add(temp);
 
                         }
                     }
